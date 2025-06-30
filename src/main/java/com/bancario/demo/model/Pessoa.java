c71a80510d6d9709801d83f58af46f6a4efb150e
Gustavo <gustavo303a@gmail.com>
2025-06-30 17:53:50 -0300

    Html do ecommerce feito e digitos do cpf arrumado.

--- src/main/java/com/bancario/demo/model/Pessoa.java
+++ src/main/java/com/bancario/demo/model/Pessoa.java
@@ -1,6 +1,8 @@
 package com.bancario.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -16,6 +18,8 @@ public class Pessoa {
     private String nome;
 
     @Column(unique = true)
+    @Size(min = 11, max = 11, message = "O CPF deve ter exatamente 11 caracteres.")
+    @Pattern(regexp = "\\d{11}", message = "O CPF deve conter apenas números.")
     private String cpf;
 
     private String password;
